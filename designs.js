6f8f6dc4e60903a341a994fc07b579de714a9f17
BMO <bmo25691@gmail.com>
2018-02-06 17:28:29 -0100

    Update designs.js

--- designs.js
+++ designs.js
@@ -3,38 +3,73 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+$(function () {
 
-  //Get nb of rows and cols input
-  var rows = $("#input_height").val();
-  var cols = $("#input_width").val();
+    //Define variables
 
-  //Get table
-  var table = $("#pixel_canvas");
+    const colorPicker = $('#color-picker');
+    let tableCanvas = $('#pixel-canvas');
+    const createButton = $('#create-grid');
+    const clearButton = $('#clear-grid');
+    let gridHeight = $('#input-height');
+    let gridWidth = $('#input-width');
 
-  //Reset to empty table --- in case one already created
-  table.children().remove();
+    let color = '#000';            // Default color of the grid.
 
-  //Create rows
-  for (var i = 0; i < rows; i++) {
-    table.append("<tr></tr>");
-    //Create cols
-    for (var j = 0; j < cols; j++) {
-      table.children().last().append("<td></td>");
+    // Select color input and save the color picked
+
+    colorPicker.change(function() {
+        color = colorPicker.val();
+        colorPicker.attr('value', color);
+    });
+
+    // Select size input after pressing createButton
+
+    createButton.click(function(evt) {
+        let N = gridHeight.val();
+        gridHeight.attr('value',N);
+        let M = gridWidth.val();
+        gridWidth.attr('value',M);
+        for(let i=1; i<=N; i++) {
+            $('#pixel-canvas tr').remove();
+        }
+
+        // Depending on the inputs, show a message alert when these are not acceptable
+
+        if((N != parseInt(N, 10)) || (M != parseInt(M, 10))) {
+            alert('Some of your inputs are not integers! Please try again.');
+        } else if((parseInt(N) <= 1) || (parseInt(M) <= 1) || (parseInt(N) > 100) || (parseInt(M) > 100)) {
+            alert('Some of your inputs are incorrect! Please try again.');
+        } else {
+            makeGrid(N,M);
+        }
+        evt.preventDefault();
+    });
+
+    //Declaration of makeGrid() function
+
+    function makeGrid(N,M) {
+        for(let i=1; i<=N; i++) {
+            tableCanvas.append('<tr></tr>');
+            for(let j=1; j<=M; j++) {
+                $('table tr:last-child').append('<td></td>');
+            }
+        }
     }
-  }
 
-  //Listen for cell clicks
-  table.on("click", "td", function() {
-    //Get color from color picker
-    var color = $("input[type='color']").val();
-    //Apply color to cell
-    $(this).attr("bgcolor", color);
-  });
-}
+    // Draw the pixel art, by clicking on the desire square.
 
-// Listen for button clicks to trigger makeGrid()
-$("input[type='submit']").click(function(e) {
-  e.preventDefault(); //Required to avoid submit and page reload
-  makeGrid();
+    tableCanvas.click(function(evt) {
+        $(evt.target).toggleClass('color-changed');       // When a cell is clicked, the "color-changed" class, associated to the color picked, is added.
+        let colorBg = $(evt.target).hasClass('color-changed') ? color : 'white';
+        $(evt.target).css('background-color', colorBg);
+    });
+
+    // Clear grid pressing button
+
+    clearButton.click(function(evt) {
+        evt.preventDefault();
+        $('td').css('background-color','white');
+    });
+
 });
